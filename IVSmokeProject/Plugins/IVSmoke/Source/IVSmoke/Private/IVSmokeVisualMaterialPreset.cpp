a1dee37206b9eb20d2cf5fd093777f36ce1dad6d
homing <k99960942@gmail.com>
2026-01-30 17:14:43 +0900

    feat: visual material preset
    [git-p4: depot-paths = "//depot/ivsmoke/": change = 569]

---
+++ IVSmokeProject/Plugins/IVSmoke/Source/IVSmoke/Private/IVSmokeVisualMaterialPreset.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2026, Team SDB. All rights reserved.
+
+#include "IVSmokeVisualMaterialPreset.h"
+#include "IVSmoke.h"
+
+#if WITH_EDITOR
+void UIVSmokeVisualMaterialPreset::PostEditChangeProperty(FPropertyChangedEvent& E)
+{
+	Super::PostEditChangeProperty(E);
+
+	const FName PropName = E.Property ? E.Property->GetFName() : NAME_None;
+
+	if (PropName == GET_MEMBER_NAME_CHECKED(UIVSmokeVisualMaterialPreset, SmokeVisualMaterial))
+	{
+		if (SmokeVisualMaterial)
+		{
+			if (UMaterial* M = SmokeVisualMaterial->GetMaterial())
+			{
+				if (M->MaterialDomain != MD_PostProcess)
+				{
+					UE_LOG(LogIVSmoke, Warning, TEXT("SmokeVisualMaterial must be PostProcess domain"));
+					SmokeVisualMaterial = nullptr;
+				}
+			}
+		}
+	}
+}
+#endif
---
+++ IVSmokeProject/Plugins/IVSmoke/Source/IVSmoke/Public/IVSmokeVisualMaterialPreset.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2026, Team SDB. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "IVSmokeVisualMaterialPreset.generated.h"
+
+/**
+ * Alpha processing type in composite pass.
+ */
+UENUM(BlueprintType)
+enum class EIVSmokeVisualAlphaType : uint8
+{
+	/** Use SmokeVisualMaterial alpha value */
+	Alpha UMETA(DisplayName = "Use Alpha (0 ~ 1)"),
+
+	/** Alpha <= AlphaThreshold ? 0 : 1 */
+	CutOff UMETA(DisplayName = "CutOff (Alpha <= AlphaThreshold ? 0 : 1)"),
+};
+
+/**
+ * Data asset containing visual material and alpha process configuration preset.
+ */
+UCLASS(BlueprintType)
+class IVSMOKE_API UIVSmokeVisualMaterialPreset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+protected:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+public:
+	/** It is used in Visual Pass, which is called after upsample filter pass */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IVSmoke | Rendering")
+	TObjectPtr<UMaterialInterface> SmokeVisualMaterial;
+
+	/** Alpha processing type in composite pass. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IVSmoke | Rendering")
+	EIVSmokeVisualAlphaType VisualAlphaType = EIVSmokeVisualAlphaType::Alpha;
+
+	/** Minimum alpha threshold for rendering. Pixels with alpha below this value will be discarded. Only used when VisualAlphaType is CutOff. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IVSmoke | Rendering", meta = (ClampMin = "0.0", ClampMax = "1.0", EditCondition = "VisualAlphaType == EIVSmokeVisualAlphaType::CutOff", EditConditionHides))
+	float AlphaThreshold = 0.0f;
+
+	/** Upper bound threshold for low-opacity remapping to suppress HDR burn-through and low-density artifacts. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "IVSmoke | Rendering", meta = (ClampMin = "0.0", ClampMax = "0.5"))
+	float LowOpacityRemapThreshold = 0.02f;
+
+
+};
